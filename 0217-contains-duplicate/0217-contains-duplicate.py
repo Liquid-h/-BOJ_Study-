9b793db239a0ba8c63ef224643aaee42f5322ea0
Liquid <yyj060313@gmail.com>
2026-05-04 10:39:04 +0900

    Time: 16 ms (32.39%), Space: 32.3 MB (26.74%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -0,0 +1,9 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        result = set()
+        for i in nums:
+            if i in result:
+                return True
+            else:
+                result.add(i)
+        return False
